27beb86403f82496c42d8731b3ba9e747952d182
KeepFobbi <52631773+KeepFobbi@users.noreply.github.com>
2021-02-17 15:52:48 +0200

    start

---
+++ script.js
@@ -0,0 +1,13 @@
+var elem = document.querySelector(".field");
+
+    var n = 4, m = 4;
+    var mas = [];
+        for (var i = 0; i < m; i++){
+            mas[i] = [];
+            for (var j = 0; j < n; j++){
+                var button = document.createElement('button');
+                button.innerHTML = i; 
+                elem.appendChild(button); 
+                mas[i][j] = ;
+        }}
+        console.log(mas);
